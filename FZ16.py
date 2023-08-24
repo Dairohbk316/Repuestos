da7842aa285387e667802c39384555a923622299
Dairo Almanza <dairohbk316@gmail.com>
2023-08-24 12:48:26 -0500

    Commit 01 repuestos

--- FZ16.py
+++ FZ16.py
@@ -2,10 +2,10 @@
 repuestos = [] # creo lista vacia
 
 def agregar_repuestos(repuestos):
-    refaccion= input('Ingrese refaccion') # Creo los valores del diccionario como variables que puede llebar el usuario
-    fecha = int(input('Ingrese fecha'))
-    km = float(input('Ingrese kilometraje'))
-    valor = float(input('ingrese valor repuesto'))
+    refaccion= input('Ingrese refaccion ') # Creo los valores del diccionario como variables que puede llebar el usuario
+    fecha = int(input('Ingrese fecha '))
+    km = float(input('Ingrese kilometraje '))
+    valor = float(input('ingrese valor repuesto '))
 
     refacciones = {
         'refaccion': refaccion, # creo el diccionario y adjunto a la clave el valor del usuario
@@ -16,4 +16,25 @@ def agregar_repuestos(repuestos):
 
     repuestos.append(refacciones) #Agrego a la lista repuestos el diccionario refacciones
     print('repuesto guradado con exito')
+
+def listar_repuestos(repuestos):
+    for i in repuestos:
+        print(i)
+
+if __name__ == '__main__':
+
+    while True:
+
+        option = input('Desea ingresar repuesto a la base s[si o no] ')
+
+        if option == 'si':
+            agregar_repuestos(repuestos)
+            listar_repuestos(repuestos)
+            
+        elif option == 'si':
+            break
+        else:
+            print('ingrese una autoparte valida')  
+
+
 
